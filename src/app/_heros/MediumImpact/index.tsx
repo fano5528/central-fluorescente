ef0a18357e0e2db29182b1b7833ab5744fe2492f
ops@fanodesign.com <ops@fanodesign.com>
2024-10-04 11:52:28 -0600

    primer commit

---
+++ src/app/_heros/MediumImpact/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Page } from '../../../payload/payload-types'
+import { Gutter } from '../../_components/Gutter'
+import { CMSLink } from '../../_components/Link'
+import { Media } from '../../_components/Media'
+import RichText from '../../_components/RichText'
+
+import classes from './index.module.scss'
+
+export const MediumImpactHero: React.FC<Page['hero']> = props => {
+  const { richText, media, links } = props
+
+  return (
+    <>
+      <RichText className="bg-red-500 text-blue-500" content={richText} />
+    </>
+  )
+}
